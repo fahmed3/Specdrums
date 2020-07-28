a1538016cbce2d49a2d074612258ae2b9623280f
Fabiha Ahmed <fsafabiha@gmail.com>
2020-07-28 18:43:49 -0400

    updated with potential createGUI func

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ let specdrums = {};
 let btn = document.getElementById("btn");
 btn.addEventListener("click", connectSpec);
 
-let multiSynth;
-
 
 //how to do success vs failure callback when the function returns before it even finishes connecting? Apparently promises only return other promises
 //
 //temporary fix by just having Specdrum.connect() return a promise which we just continue to run with
 function connectSpec() {
-    multiSynth = new MichaelMultiSynth();
     //console.log(multiSynth.key);
     Specdrum.connect()
 	.then(success, failure)
@@ -21,27 +18,28 @@ function connectSpec() {
 }
 
 function success(spec){
+    let multiSynth = new MichaelMultiSynth();
     console.log("Connected New Specdrum: ", spec.device.name);
     specdrums[spec.device.id] = spec;
     //console.log(specdrums);
     console.log(spec);
     console.log("Success!");
-    spec.addEventListener("tap", tapped);
-    spec.addEventListener("release", released);	   
+    spec.addEventListener("tap", (e) => multiSynth.attack(e.note, e.isChord) );
+    spec.addEventListener("tap", (e) => console.log("tapped") );
+    spec.addEventListener("release", (e) => multiSynth.release(e.isChord) );
+    createGUI(spec, multiSynth);
 }
+
 function failure() {
     console.log("Failed to connect");
 }
 
-//specdrum.addEventListener("tap", tapped);
-//specdrum.addEventListener("release", released);
-
-function tapped(e) {
-    console.log("Tap");
-    multiSynth.attack(e.note, e.isChord);
-}
-
-function released(e) {
-    console.log("Release");
-    multiSynth.release(e.isChord);
+function createGUI(spec, multisynth){
+    //create new synth gui
+    let synths = document.getElementById("synths");
+    let p = document.createElement("p");
+    p.innerHTML = spec.device.name;
+    synths.append(p);
+    //create gui elements
+    //make sure sliders affect parameters of multisynth - volume    
 }
